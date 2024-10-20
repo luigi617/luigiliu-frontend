3e23cfa516b74883aee2297bb983e663274298ed
luigi617 <luigiliu617@gmail.com>
2024-10-20 17:47:40 -0400

    article games

--- src/pages/Articles.tsx
+++ src/pages/Articles.tsx
@@ -1,9 +1,61 @@
 import React from 'react';
+import testImage from '../assets/images/ds.jpg';
+import styles from '../css/Articles.module.scss';
+import ArticleCard from '../components/ArticleCard';
+
+const articles = [
+  {
+    image: testImage,
+    title: 'DATA STRUCTURE AND ALGORITHM CHEAT SHEET',
+    category: 'Programming',
+    date: 'Jun 26 2024',
+  },
+  {
+    image: testImage,
+    title: 'EIGENVALUES AND EIGENVECTORS',
+    category: 'Mathematics',
+    date: 'Dec 21 2023',
+  },
+  {
+    image: testImage,
+    title: 'LINEAR REGRESSION',
+    category: 'Programming',
+    date: 'Dec 19 2023',
+  },
+  {
+    image: testImage,
+    title: 'LINEAR REGRESSION',
+    category: 'Programming',
+    date: 'Dec 19 2023',
+  },
+  {
+    image: testImage,
+    title: 'LINEAR REGRESSION',
+    category: 'Programming',
+    date: 'Dec 19 2023',
+  },
+  {
+    image: testImage,
+    title: 'LINEAR REGRESSION',
+    category: 'Programming',
+    date: 'Dec 19 2023',
+  },
+];
 
 const Articles: React.FC = () => {
   return (
-    <div>
-      <h1>Articles</h1>
+    <div className={styles.articleContainer}>
+        <div className={styles.articleGrid}>
+          {articles.map((article, index) => (
+            <ArticleCard
+              key={index}
+              image={article.image}
+              title={article.title}
+              category={article.category}
+              date={article.date}
+            />
+          ))}
+        </div>
     </div>
   );
 };
--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import GameCard from '../components/GameCard';
+import testImage from '../assets/images/ds.jpg';
+import styles from '../css/Game.module.scss'
 
 const Games: React.FC = () => {
   return (
-    <div>
-      <h1>Games</h1>
+    <div className={styles.gameContainer}>
+        <div className={styles.gameGrid}>
+            <GameCard
+            image={testImage}
+            title="Awesome Game Title"
+            />
+            <GameCard
+            image={testImage}
+            title="Awesome Game Title"
+            />
+            <GameCard
+            image={testImage}
+            title="Awesome Game Title"
+            />
+        </div>
     </div>
   );
 };
