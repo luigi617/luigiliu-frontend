e428ab00db43ee96ed56f74929ddcfc578fb0297
luigi617 <luigiliu617@gmail.com>
2025-01-09 21:11:05 +0100

    fix

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -7,35 +7,56 @@ import tictactoeImage from '../assets/images/games/Tic_tac_toe.svg';
 import nbaImage from '../assets/images/games/nba-logo.jpg';
 import styles from '../css/Games.module.scss'
 
+import { isDesktop } from 'react-device-detect';
+
+const games = [
+  {
+    image: game2048Image,
+    title: "2048",
+    url: '/games/2048',
+    show: true
+  },
+  {
+    image: typingImage,
+    title: "Typing",
+    url: '/games/typing',
+    show: isDesktop
+  },
+  {
+    image: restaurantsImage,
+    title: "Nearby Restaurants",
+    url: '/games/nearby-restaurants',
+    show: true
+  },
+  {
+    image: tictactoeImage,
+    title: "TicTacToe",
+    url: '/games/tictactoe',
+    show: true
+  },
+  {
+    image: nbaImage,
+    title: "NBA",
+    url: '/games/nba',
+    show: true
+  },
+]
+
+const visibleGames = games.filter(game => game.show);
+
 const Games: React.FC = () => {
   return (
     <div className={styles.gameContainer}>
         <div className={styles.gameGrid}>
+          {visibleGames && visibleGames.map((game, index) => (
             <GameCard
-            image={game2048Image}
-            title="2048"
-            url='/games/2048'
+            key={index}
+            image={game.image}
+            title={game.title}
+            url={game.url}
             />
-            <GameCard
-            image={typingImage}
-            title="Typing"
-            url='/games/typing'
-            />
-            <GameCard
-            image={restaurantsImage}
-            title="Nearby Restaurants"
-            url='/games/nearby-restaurants'
-            />
-            <GameCard
-            image={tictactoeImage}
-            title="TicTacToe"
-            url='/games/tictactoe'
-            />
-            <GameCard
-            image={nbaImage}
-            title="NBA"
-            url='/games/nba'
-            />
+          ))}
+           
         </div>
     </div>
   );
